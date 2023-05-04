6805a810dc7296b74245d227b5b2aa79b72ff515
Trần  Khoa <trankhoa@Trans-MacBook-Pro.local>
2023-05-04 16:06:31 +0700

    feat: add isLoading for button [002]

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -114,7 +114,12 @@ const LandingSection = () => {
                 />
                 <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
               </FormControl>
-              <Button type="submit" colorScheme="purple" width="full">
+              <Button
+                type="submit"
+                colorScheme="purple"
+                width="full"
+                isLoading={isLoading}
+              >
                 Submit
               </Button>
             </VStack>
